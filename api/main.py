f4c0cbb4aba80946f6bfd34e04f09f6ac59979d6
Akash Sahu <asahu7744@gmail.com>
2022-05-28 19:12:14 +0530

    courses api completed

--- api/main.py
+++ api/main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -7,6 +8,13 @@ app = FastAPI()
 @app.get("/")
 def hello_server():
     return {"msg": "Hello World"}
+
+
+class Course(BaseModel):
+    id: int
+    name: str
+    price: float
+    is_early_bird: Optional[bool] = None
 
 
 fakedb = []
@@ -20,3 +28,15 @@ def getCourses():
 @app.get("/courses/{course_id}")
 def getCourse(course_id: int):
     return fakedb[course_id-1]
+
+
+@app.post("/courses")
+def addCourse(course: Course):
+    fakedb.append(course.dict())
+    return {"msg": "course added successfully"}
+
+
+@app.delete("/courses/{course_id}")
+def deleteCourse(course_id: int):
+    fakedb.pop(course_id)
+    return {"msg": "course deleted successfully"}

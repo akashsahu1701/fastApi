1588dcfefe7272ec36658fe2384146f10cf5ce7c
akashsahu1701 <asahu7744gmail.com>
2022-07-12 22:49:57 +0530

    changing url

--- api/database.py
+++ api/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgres://sqvjrbqpdsqgbo:a38001e9465f171fa8f23f3d5ba58bd195d535ce16794e0f35bfbeba6f905a21@ec2-52-20-166-21.compute-1.amazonaws.com:5432/d94f5sfbv578fk'
+SQLALCHEMY_DATABASE_URL = 'postgresql://sqvjrbqpdsqgbo:a38001e9465f171fa8f23f3d5ba58bd195d535ce16794e0f35bfbeba6f905a21@ec2-52-20-166-21.compute-1.amazonaws.com:5432/d94f5sfbv578fk'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
